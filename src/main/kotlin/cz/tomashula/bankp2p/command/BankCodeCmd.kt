4131adf898dc4872ff856cb9e54a24a5dee31d5c
Tomas Hula <tomashula06@gmail.com>
2025-01-30 13:04:01 +0100

    Add build to BankCodeCmd

--- src/main/kotlin/cz/tomashula/bankp2p/command/BankCodeCmd.kt
+++ src/main/kotlin/cz/tomashula/bankp2p/command/BankCodeCmd.kt
@@ -2,10 +2,17 @@ package cz.tomashula.bankp2p.command
 
 class BankCodeCmd(
     private val bankCode: String,
-) : Command("BC")
+) : Command(NAME)
 {
     override fun execute(args: List<String>): String
     {
         return bankCode
+    }
+
+    companion object
+    {
+        private const val NAME = "BC"
+
+        fun build() = NAME
     }
 }
